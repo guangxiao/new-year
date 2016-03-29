c1e863d66fc03921394af610032e027bc53ebb52
guangxiao <guangxiao.chen@dian.fm>
2016-03-29 15:15:43 +0800

    页面切换

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 window.onload = function() {
+    var page1 = document.getElementById("page1");
+    var page2 = document.getElementById("page2");
+    var page3 = document.getElementById("page3");
+
     var music = document.getElementById("music");
     var audio = document.getElementsByTagName("audio")[0];
 
@@ -8,7 +12,6 @@ window.onload = function() {
         // music.style.animationPlayState = "paused";
         // music.style.webkitAnimationPlayState = "paused";
     }, false);
-
 
     // 点击音乐图标，控制音乐播放
     // music.onclick = function() {
@@ -34,6 +37,24 @@ window.onload = function() {
              audio.pause();
              this.setAttribute("class", "");
         };
+    }, false);
+
+    /**
+     * 翻页
+     */
+
+    page1.addEventListener("touchstart", function(event) {
+        page1.style.display = "none";
+        page2.style.display = "block";
+        page3.style.display = "block";
+        page3.style.top     = "100%";
+
+        setTimeout(function (){
+            page2.setAttribute("class", "page fadeOut");
+            page3.setAttribute("class", "page fadeIn");
+
+        }, 5500);
+
     }, false);
 
 };
